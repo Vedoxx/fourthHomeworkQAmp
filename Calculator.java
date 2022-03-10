3142fd88755eba79ab2b2acdac72d3d9068278e5
Vedad Karalić <61968590+Vedoxx@users.noreply.github.com>
2022-03-10 10:45:12 +0100

    Update Calculator.java

--- Calculator.java
+++ Calculator.java
@@ -1,21 +1,20 @@
 public class Calculator {
     private final float firstNumber;
     private final float secondNumber;
-    private final double  pi;
+    private static final double  pi=Math.PI;
     private final float radius;
 
     Calculator(int firstNumber,int secondNumber,int radius){
     this.firstNumber=firstNumber;
     this.secondNumber=secondNumber;
     this.radius=radius;
-    this.pi=Math.PI;
     }
 
     public float addingTwoNumbers(){
     return this.firstNumber+this.secondNumber;
     }
 
-    public float addingNNumbers(int nnumber){
+    public float addingNNumbers(int nnumber){  
         return nnumber;
     }
 
